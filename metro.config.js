80a4138336ffa12e4a7fd51e835aee918610d4f6
Lorrany Abrantes <abranteslorrany@outlook.com>
2022-09-12 11:20:54 -0300

    add expo updates

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = {
+  transformer: {
+    assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+  },
+  ...getDefaultConfig(__dirname),
+};
